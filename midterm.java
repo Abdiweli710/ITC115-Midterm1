47bcd6c0ac09b00f348ebbb680d1030b200b2b87
Abdiweli710 <60114100+Abdiweli710@users.noreply.github.com>
2020-02-24 14:37:35 -0800

    Update midterm.java

--- midterm.java
+++ midterm.java
@@ -1,6 +1,4 @@
-// this program includes the class declaration etc so that it can run – this isn’t // needed for a correct answer
-import java.util.*;
-   public class Ant {
+public class Ant {
        public static void main(String[] args) {
 Random rand = new Random(); int height = 0;
 int falls = 0;
